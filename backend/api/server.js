ea42f5bb26fe032480564d6f6884b4e38df97f4a
sanathmi21 <ar112759@fhss.sjp.ac.lk>
2025-12-09 22:04:00 +0530

    Update CORS for frontend connection

--- backend/api/server.js
+++ backend/api/server.js
@@ -15,8 +15,12 @@ const app = express();
 
 // Middlewares 
 app.use(cors({
-  origin: "*", 
-  methods: ["GET", "POST", "PUT", "DELETE"]
+  origin: [
+    "http://localhost:5173", 
+    "https://fin-sync-project-frontend.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
 }));
 app.use(express.json());
 
